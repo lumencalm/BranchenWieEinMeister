e6f4b3212efeb91a24ad3c8830c6fbbf734a0c24
lumencalm <M.Haas@htlvb.at>
2025-09-22 08:16:13 +0200

    new cw with deep talk information

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Neuer Morgen bringt Kummer und Sorgen (❁´◡`❁)");
+Console.WriteLine("-Manuel Haas-");
+Console.WriteLine("Alles an diesem Tag ist wunderbar und schön");
+Console.WriteLine("---------------------------------------------");
